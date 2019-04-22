1678f27d05d44b1a7f9ee2776a68ca03ee18ec10
Michael Bryan <michaelfbryan@gmail.com>
2019-04-23 02:07:57 +0800

    Created a light that points at a slowly moving ball

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,48 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import {
+    Scene, PerspectiveCamera, WebGLRenderer, SphereGeometry,
+    MeshLambertMaterial, Mesh, AmbientLight, PointLight,
+} from 'three/src/Three.js';
 import './master.css';
 
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({ antialias: true });
+renderer.setClearColor("#e5e5e5");
 
-const scene: any = new Scene();
-const camera: any = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube: any = new Mesh(geometry, material);
-scene.add(cube);
-
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 camera.position.z = 5;
+
+const geometry = new SphereGeometry(1, 10, 10);
+const material = new MeshLambertMaterial({ color: 0xffcc00 });
+const ball = new Mesh(geometry, material);
+scene.add(ball);
+
+const ambient = new AmbientLight(0x404040, 0.5);
+scene.add(ambient);
+
+const light = new PointLight(0xffffff, 0.8, 500);
+light.position.set(10, 0, 25);
+light.lookAt(ball.position);
+scene.add(light);
+
 
 function updateDimensions() {
     renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 }
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    ball.rotation.x += 0.01;
+    ball.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
-addEventListener('load', () => {
+function initialize() {
     updateDimensions();
     document.body.appendChild(renderer.domElement);
-    animate();
-})
+    requestAnimationFrame(animate);
+}
+
+addEventListener('load', initialize);
+addEventListener('resize', updateDimensions);
